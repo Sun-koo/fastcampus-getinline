2f9c6cdd0bb2bfedad2313033a10657c09af818c
suku0307 <suku0307@gmail.com>
2023-06-22 17:29:09 +0900

    Initialize spring boot
    스프링 부트 기본 설정

---
+++ src/main/java/com/fastcamp/getinline/FastcampusGetinlineApplication.java
@@ -0,0 +1,13 @@
+package com.fastcamp.getinline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcampusGetinlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastcampusGetinlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fastcamp/getinline/FastcampusGetinlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcamp.getinline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastcampusGetinlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
