f1f09a13bbc31579302c3147e20a942b8010cc41
suku0307 <suku0307@gmail.com>
2023-06-28 12:21:11 +0900

    Add #2 - RepositoryConfig
    EventRepository 테스트를 위해 구현
    나중에 필요 없어짐

---
+++ src/main/java/com/fastcamp/getinline/config/RepositoryConfig.java
@@ -0,0 +1,14 @@
+package com.fastcamp.getinline.config;
+
+import com.fastcamp.getinline.repository.EventRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RepositoryConfig {
+
+    @Bean
+    public EventRepository eventRepository() {
+        return new EventRepository() {};
+    }
+}
